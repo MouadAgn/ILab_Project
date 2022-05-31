092e177f00778c539c0b5452e4bc5c51f0755c0c
Mouad <103049879+MouadAgn@users.noreply.github.com>
2022-05-31 02:38:23 +0200

    New Changes
    I have created a new route (new page TestTable.blade.php which contains PCR/Results)
    To access to this page just type in the URL after the route ' /Results  '
    
    And i have created a 404 Error Page paege404.blade.php
    
    make sure to install what i told you on discord

---
+++ resources/views/TestTable.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TestResults</title>
+    
+    {{-- Scripts --}}
+    <script src="{{ asset('js/app.js') }}" ></script>
+    
+    {{-- Styles --}}
+    <link  rel="stylesheet"" href="{{ asset('css/app.css') }}">
+
+
+</head>
+<body>
+    <h1 class="text-success">Test Results</h1>
+
+    <br>
+
+    <div class="container">
+                      
+                                
+        <div class="row" style="margin-top : 300px;">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="card-title m-b-0">PCR Results</h5>
+                </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox m-b-20">
+                                            <input type="checkbox" id="mainCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">DNI</th>
+                                    <th scope="col">Telefono</th>
+                                    <th scope="col">Motivo</th>
+                                    <th scope="col">Pres</th>
+                                    <th scope="col">Plantilla</th>
+                                    <th scope="col">NR.Muestra</th>
+                                    <th scope="col">Pin</th>
+                                    <th scope="col">N.Autorizacion</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Email</th>
+                                </tr>
+                            </thead>
+                            <tbody class="customtable">
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <label class="customcheckbox">
+                                            <input type="checkbox" class="listCheckbox">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </th>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                    <td>Test</td>
+                                </tr>
+
+                                
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+       </div>           
+
+  
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//   return view('welcome');
+//});
+
+ Route::view('/','welcome')->name('home');
+
+ /*Route::match(['get','post'], '/user/profile', function()
+ {
+
+ });
+
+ Route::any('users/{id}', function($id)
+ {
+
+ });
+
+Route::redirect('/etudiant', '/', 301);*/
+
+/*Route::get('users/{id}', function($id)
+{
+    return 'Hello Mouad' .$id;
+})->where(['id' => '[0-9]+'])->name('user');*/
+
+/*Route::get('users/{id?}', function($id = null)
+{
+    return 'Hello Mouad' .$id;
+})->name('user');*/
+
+//$url = Route('user');
+
+/*Route::prefix('admin')->group(function()
+{
+    Route::get('users/{id}', function($id)
+    {
+        return 'Hello Mouad' . $id;
+    });
+});*/
+
+/*Route::name('admin.')->group(function()
+{
+    Route::get('users/{id}', function($id)
+    {
+        return 'Hello Mouad' . $id;
+    })->name('users');
+});*/
+
+Route::fallback(function(){
+    return view('page404');
 });
+
+Route::get('users/{name}', function ($name)
+{
+    return view('users',['ObjJSON' => ['name'=>'Mouad', 'age'=>'22']]);
+});
+
+/*Route::get('test/', function ()
+{
+    return['name'=>'Mouad', 'age'=>'22'];
+});*/
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/test','test');
+
+Route::view('/Results','TestTable');
